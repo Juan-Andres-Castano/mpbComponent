8481267f2895e13dd41db5abe3bda7678dee3a4c
juan.castano <juan.castano@mpbc.ca>
2022-01-25 16:21:22 -0500

    sec

--- List.cmd
+++ List.cmd
@@ -3,5 +3,15 @@
 Rem This is for listing down all the files in the directory Program files 
 cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\configurationManagement "
 dir
+del *.xml
+del *.txt
+
+cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
+ceedling test:all
+
+cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
+
+copy  C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test\report.xml c:\mpbComponent\configurationManagement\
+
 echo "The program has completed"
 
