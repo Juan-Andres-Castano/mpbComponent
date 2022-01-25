a685033995a11533efd3057a0d9d64437697121e
juan.castano <juan.castano@mpbc.ca>
2022-01-25 16:37:23 -0500

    sec

--- List.cmd
+++ List.cmd
@@ -2,14 +2,17 @@
 @echo off 
 Rem This is for listing down all the files in the directory Program files 
 cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\configurationManagement "
-dir
+echo " changinfg to c:\mpbComponent\configurationManagement\ " 
 cd " c:\mpbComponent\configurationManagement\ " 
+dir
 Rem C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
 Rem del *.xml
 Rem del *.txt
 
 Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
+echo " changing to c:\mpbComponent\mpbTime\unittestframework\ " 
 cd " c:\mpbComponent\mpbTime\unittestframework\ " 
+dir
 ceedling test:all
 
 cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
