814305e56c582d30fcefe9f583cdc415f935afb2
juan.castano <juan.castano@mpbc.ca>
2022-01-25 16:40:33 -0500

    sec

--- List.cmd
+++ List.cmd
@@ -1,21 +1,21 @@
 
 @echo off 
 Rem This is for listing down all the files in the directory Program files 
-cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\configurationManagement "
-echo " changinfg to c:\mpbComponent\configurationManagement\ " 
-cd " c:\mpbComponent\configurationManagement\ " 
-dir
+Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\configurationManagement "
+Rem echo " changinfg to c:\mpbComponent\configurationManagement\ " 
+Rem cd " c:\mpbComponent\configurationManagement\ " 
+
 Rem C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
 Rem del *.xml
 Rem del *.txt
 
 Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
-echo " changing to c:\mpbComponent\mpbTime\unittestframework\ " 
-cd " c:\mpbComponent\mpbTime\unittestframework\ " 
-dir
-ceedling test:all
+echo "executing changing to c:\mpbComponent\mpbTime\unittestframework\ " 
+cd " c:\mpbComponent\mpbTime\unittestframework\ceedling test:all " 
 
-cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
+
+
+Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
 
 copy  C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test\report.xml c:\mpbComponent\configurationManagement\
 
