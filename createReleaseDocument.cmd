00984568b3d52ce068edcc1cb0cfcbc5cbcdcef9
juan.castano <juan.castano@mpbc.ca>
2022-01-27 15:51:45 -0500

    createReleasedocument batch file updated. versions fixed

--- createReleaseDocument.cmd
+++ createReleaseDocument.cmd
@@ -38,29 +38,74 @@ echo .... >> templogFile.txt
 Rem Set mpbTimeLibraryVersion = "mpbTimeLibraryVersion"
 Rem Set mpbTimeLibraryValue = %2
 
-cd "C:\mpbComponent\mpbTime\Release"
-set /p Build =< version.txt
-echo %Build%
+Rem cd "C:\mpbComponent\mpbTime\Release\"
+cd "C:\mpbComponent\mpbMath\Release\"
 
+SET var=
+
+for /f "delims=" %%i in (version.txt) do call :setVar %%i
+echo final var = %var%
+goto exit
+
+:exit
+
+
+Rem set /p Build =< version.txt
+Rem echo %Build%
+Rem dir
+Rem pause
+
+cd "C:\mpbComponent\mpbTime\Release\"
+Rem dir
+Rem pause
+
+SET var1=
+
+for /f "delims=" %%i in (version.txt) do call :setVar1 %%i
+echo final var1 = %var1%
+goto exit1
+
+:exit1
+
+
+Rem set /p out =< version.txt
+Rem echo %Build1%
+
+Rem dir
+Rem pause
+
+
+
+cd "C:\mpbComponent\configurationManagement"
 
 
 echo mpbTimeLibraryVersion >> templogFile.txt
-echo %Build% >> templogFile.txt
+echo %var1% >> templogFile.txt
 echo .... >> templogFile.txt
 
 
 Rem Set mpbMathLibraryVersion = "mpbMathLibraryVersion"
 Rem Set mpbMathLibraryValue = %3
 
-cd "C:\mpbComponent\mpbMath\Release"
-set /p mpbTimeVersion =< version.txt
-echo %mpbTimeVersion%
 
 
+Rem dir
+Rem pause
+
+cd "C:\mpbComponent\configurationManagement"
 echo mpbMathLibraryVersion >> templogFile.txt
-echo %mpbTimeVersion% >> templogFile.txt
+echo %var% >> templogFile.txt
 echo .... >> templogFile.txt
 Rem echo.>>templogFile.txt
 
 
 ren  c:\mpbComponent\configurationManagement\templogFile.txt softwareReleaseDocument_buildNumber_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%time:~0,2%h%time:~3,2%m%time:~6,2%s%.txt
+
+
+:SetVar
+Set var=%var% %1%
+goto :eof
+
+:SetVar1
+Set var1=%var1% %1%
+goto :eof
