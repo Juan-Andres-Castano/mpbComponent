bcb711fa6d2964457655a98ec59ca38f1ac7eee1
juan.castano <juan.castano@mpbc.ca>
2022-01-27 11:40:48 -0500

    bash treatement rename result files

--- mpbMathTest.cmd
+++ mpbMathTest.cmd
@@ -8,6 +8,8 @@ Rem cd " c:\mpbComponent\configurationManagement\ "
 Rem C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
 Rem del *.xml
 Rem del *.txt
+echo "parameter 1 *....."
+echo %1
 
 Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
 echo "executing unit testing....."
