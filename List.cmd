f3b206e9991eb09f6ac09b2b26c90abcb380669f
juan.castano <juan.castano@mpbc.ca>
2022-01-27 10:42:03 -0500

    batch file update

--- List.cmd
+++ List.cmd
@@ -5,7 +5,7 @@ Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\configurationM
 Rem echo " changinfg to c:\mpbComponent\configurationManagement\ " 
 Rem cd " c:\mpbComponent\configurationManagement\ " 
 
-Rem C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
+Rem  C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework "
 Rem del *.xml
 Rem del *.txt
 
@@ -19,7 +19,7 @@ ceedling
 
 Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
 
-Rem copy  C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test\report.xml c:\mpbComponent\configurationManagement\
+copy  C:\mpbComponent\mpbTime\UnitTestFramework\build\artifacts\test\report.xml c:\mpbComponent\configurationManagement\report_mpbTime.xml
 
 echo "The program has completed"
 
