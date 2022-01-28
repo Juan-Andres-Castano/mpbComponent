bd47b4a7280671958760fc61a4a94ecf397fda4c
juan.castano <juan.castano@mpbc.ca>
2022-01-28 14:28:24 -0500

    bash treatement rename result files

--- mpbTimeReport.cmd
+++ mpbTimeReport.cmd
@@ -28,10 +28,23 @@ set d=%d: =_%
 set t=%time:~0,2%h%time:~3,2%m%time:~6,2%s%
 set t=%t: =0%
 
+
+cd "C:\mpbComponent\mpbTime\Release\"
+SET var2=
+for /f "delims=" %%i in (version.txt) do call :setVar2 %%i
+::echo final var2=%var2%
+set var2=%var2: =%
+::echo final var2 no spaces=%var2%
+
+set "variable=%1_V_%var2%"
+::echo %variable%
+set vari=%variable%
+
+
 Rem RENAME "W:\TEST.TXT" "TEST_%d%_%t%.TXT"
 Rem ren c:\mpbComponent\configurationManagement\report.xml report_%1_%d%_%t%.xml
 
-ren c:\mpbComponent\mpbTime\Tests\reports\report.xml report_mpbTime_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%t%.xml
+ren c:\mpbComponent\mpbTime\Tests\reports\report.xml report_mpbTime_%1_V_%var2%_@_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%t%.xml
 Rem %time:~0,2%h%time:~3,2%m%.xml 
 Rem %time:~0,2%h%time:~3,2%m%time:~6,2%s%.xml
 Rem report_mpbTime_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%time:~0,2%h%time:~3,2%m%time:~6,2%s%.xml
@@ -56,3 +69,6 @@ Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unitte
 ::xcopy  "C:\mpbComponent\mpbTime\UnitTestFramework\build\artifacts\test\*.*" "c:\mpbComponent\configurationManagement\." /i /e
 
 ::ren  c:\mpbComponent\configurationManagement\report.xml report_mpbTime.xml
+:SetVar2
+Set var2=%var2% %1%
+goto :eof
