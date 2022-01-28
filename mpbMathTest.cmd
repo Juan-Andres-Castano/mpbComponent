ec32fced62f8f95f60678d64d1ccb9bc75da38b3
juan.castano <juan.castano@mpbc.ca>
2022-01-28 09:54:53 -0500

    bash treatement rename result files

--- mpbMathTest.cmd
+++ mpbMathTest.cmd
@@ -17,11 +17,11 @@ echo "executing unit testing....."
 Rem cd  "c:\mpbComponent\mpbTime\unittestframework\" 
 Rem del c:\mpbComponent\configurationManagement\report.xml
 
-pause
+
 
 xcopy  "C:\mpbComponent\mpbTime\UnitTestFramework\build\artifacts\test\*.*" "c:\mpbComponent\configurationManagement\." /i /e 
 
-pause
+
 
 set d=%date:~-4,4%-%date:~-7,2%-%date:~0,2%
 set d=%d: =_%
@@ -31,18 +31,18 @@ set t=%t: =0%
 Rem RENAME "W:\TEST.TXT" "TEST_%d%_%t%.TXT"
 Rem ren c:\mpbComponent\configurationManagement\report.xml report_%1_%d%_%t%.xml
 
-ren c:\mpbComponent\configurationManagement\report.xml report_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%t%.xml
+ren c:\mpbComponent\configurationManagement\report.xml report_mpbTime_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%t%.xml
 Rem %time:~0,2%h%time:~3,2%m%.xml 
 Rem %time:~0,2%h%time:~3,2%m%time:~6,2%s%.xml
 Rem report_mpbTime_%1_%date:~4,2%-%date:~7,2%-%date:~10,4%_@_%time:~0,2%h%time:~3,2%m%time:~6,2%s%.xml
-pause
+
 Rem cd  "c:\mpbComponent\mpbMath\unittestframework\" 
 Rem ceedling test:all
 
 
 cd  "c:\mpbComponent\mpbTime\unittestframework\" 
 ceedling 
-pause
+
 Rem cd "C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test"
 
 Rem copy  C:\ProgramData\Jenkins\.jenkins\workspace\pipelinegitdemo\mpbTime\unittestframework\build\artifacts\test\report.xml c:\mpbComponent\configurationManagement\
